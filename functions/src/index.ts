88d5c9dfb96db56d5d475b0f1019708f45174900
Kevin Roa <kevinroa101@gmail.com>
2021-01-03 18:16:49 -0400

    remove unnecessary dependancy, worked on pdf deletion from storage

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,5 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-import { Storage } from '@google-cloud/storage';
 import { ObjectMetadata } from 'firebase-functions/lib/providers/storage';
 
 admin.initializeApp(functions.config().firebase);
@@ -9,9 +8,16 @@ admin.initializeApp(functions.config().firebase);
 export const saveToDrive = functions.storage
 	.object()
 	.onFinalize((object: ObjectMetadata) => {
-		const gcs = new Storage();
-
-		const bucket = gcs.bucket(object.bucket);
+		const bucket = admin.storage().bucket(object.bucket);
 		const fullFileName = object.name;
-		const fileName = fullFileName?.split('.zip')[0];
+		// const fileName = fullFileName?.split('.pdf')[0];
+		// const filePath = `gs://${bucket}/${fullFileName}`;
+
+		// Only run code when a pdf is added to the bucket
+		if (fullFileName?.includes('.pdf')) {
+			// Upload to google drive
+
+			// Remove pdf from cloud bucket
+			bucket.file(fullFileName).delete();
+		}
 	});
