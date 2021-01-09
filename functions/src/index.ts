fbb25c52be0f449ab51b37fdea06fae838029432
Kevin Roa <kevinroa101@gmail.com>
2021-01-09 01:36:33 -0400

    Restructure how uploaded files will be managed on google cloud

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,130 +7,45 @@ import { ObjectMetadata } from 'firebase-functions/lib/providers/storage';
 
 admin.initializeApp(functions.config().firebase);
 
-// Listen for object changes on default storage bucket
-// export const saveToDrive = functions.storage
-// 	.object()
-// 	.onFinalize((object: ObjectMetadata) => {
-// 		const bucket = admin.storage().bucket(object.bucket);
-// 		const fullFileName = object.name;
-// 		// const fileName = fullFileName?.split('.pdf')[0];
-// 		// const filePath = `gs://${bucket}/${fullFileName}`;
-// 		const file = bucket.file(fullFileName!);
-
-// 		// Only run code when a pdf is added to the bucket
-// 		if (fullFileName?.includes('.pdf')) {
-// 			// Get the file data
-// 			file.get().then((data) => {
-// 				// Upload to google drive
-// 				console.log(data[0]);
-// 			});
-
-// 			// Remove pdf from cloud bucket
-// 			return file.delete();
-// 		} else {
-// 			console.log('Uploaded file is not a PDF.');
-// 			return null;
-// 		}
-// 	});
-
-// export const savePdfToDrive = functions.https.onRequest((req, res) => {
-// 	res.header('Access-Control-Allow-Origin', '*');
-// 	res.header(
-// 		'Access-Control-Allow-Headers',
-// 		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-// 	);
-// 	if (req.method === 'OPTIONS') {
-// 		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-// 		res.status(200).json({});
-// 	} else {
-// 		console.log('0');
-
-// 		// axios
-// 		// 	.post(
-// 		// 		'https://oauth2.googleapis.com/token',
-// 		// 		{
-// 		// 			client_id: creds.web.client_id,
-// 		// 			client_secret: creds.web.client_secret,
-// 		// 			refresh_token: req.body.token,
-// 		// 			grant_type: 'refresh_token'
-// 		// 		},
-// 		// 		{
-// 		// 			headers: {
-// 		// 				'Content-Type': 'application/x-www-form-urlencoded'
-// 		// 			}
-// 		// 		}
-// 		// 	)
-// 		// 	.then((res) => {
-// 		// 		console.log(res);
-// 		// 	});
-
-// 		const access_token = '';
-
-// 		axios.get(
-// 			`https://www.googleapis.com/drive/v3/files?access_token=${access_token}`
-// 		);
-
-// 		// const drive = google.drive({ version: 'v3' });
-// 		// console.log('1');
-// 		// drive.files.list(
-// 		// 	{
-// 		// 		pageSize: 10,
-// 		// 		fields: 'nextPageToken, files(id, name)'
-// 		// 	},
-// 		// 	(err, res) => {
-// 		// 		if (err) return console.log('The API returned an error: ' + err);
-// 		// 		const files = res!.data.files;
-// 		// 		if (files!.length) {
-// 		// 			console.log('Files:');
-// 		// 			files!.map((file) => {
-// 		// 				console.log(`${file.name} (${file.id})`);
-// 		// 			});
-// 		// 		} else {
-// 		// 			console.log('No files found.');
-// 		// 		}
-// 		// 	}
-// 		// );
-// 	}
-// 	res.status(200).json({
-// 		data: req.body
-// 	});
-// });
-
 exports.scanDocument = functions.storage
 	.object()
 	.onFinalize(async (object: ObjectMetadata) => {
 		const bucketName = object.bucket;
 		const fileName = object.name;
 
-		const bucket = admin.storage().bucket(bucketName);
-		const file = bucket.file(fileName!);
-		const [fileMetadata] = await file.getMetadata();
-		const owner = fileMetadata.metadata.owner;
+		// const bucket = admin.storage().bucket(bucketName);
+		// const file = bucket.file(fileName!);
 
 		const fileUri = `gs://${bucketName}/${fileName}`;
-		const fileDestUri = `gs://${bucketName}/documents/${owner}`;
+		const fileDestUri = `gs://${bucketName}/${fileName?.split('.')[0]}`;
 
-		console.log(fileName);
-
-		// Only run when the file is a pdf and not in the documents folder
-		if (fileName?.includes('documents/')) {
-			return;
-		} else if (fileName?.includes('.pdf')) {
-			await ocr(file, fileUri, fileDestUri);
-
-			// Delete old file once everything is done
-			console.log('Deleting file.');
-			file.delete();
+		// Case for the OCR output file
+		// Run if name has 'output' and has the .json ending
+		if (['output', '.json'].every((val) => fileName?.includes(val))) {
+			// Delete old file once metadata is added to the pdf
+			// console.log('Deleting file.');
+			// file.delete();
 		}
+		// Case for the uploaded pdf file
+		// Run if uploaded to a folder and has the .pdf ending
+		else if (['/', '.pdf'].every((val) => fileName?.includes(val))) {
+			await ocr(fileUri, fileDestUri);
+		}
+		// Invalid file
+		// Uploaded to main directory
+		// Not named properly
+		// Not a .pdf or .json
+		else console.log('Invalid upload.');
 	});
 
 // Get a string of text from the given pdf file
 // Call the callback with the text
-async function ocr(file: any, fileUri: String, fileDestUri: String) {
+async function ocr(fileUri: String, fileDestUri: String) {
 	console.log('Running Vision OCR on "' + fileUri + '".');
 
 	const client = new vision.ImageAnnotatorClient();
 
+	// Setup for Vision API call
 	const inputConfig = {
 		mimeType: 'application/pdf',
 		gcsSource: {
@@ -153,7 +68,12 @@ async function ocr(file: any, fileUri: String, fileDestUri: String) {
 		]
 	} as vision.protos.google.cloud.vision.v1.IAsyncBatchAnnotateFilesRequest;
 
+	// Call Vision API
 	const [operation] = await client.asyncBatchAnnotateFiles(request);
+
+	// Wait for OCR to complete then log the destination
+	// Vision asyncBatchAnnotateFiles can only output to a
+	//	cloud storage location
 	const [filesResponse] = await operation.promise();
 	console.log(
 		'Saved Vision OCR result to: "' +
--- src/composables/usePhotos.ts
+++ src/composables/usePhotos.ts
@@ -23,28 +23,19 @@ export function usePhotos() {
 	const photos = ref<Photo[]>([]);
 
 	const uploadPdf = (pdf: jsPDF) => {
-		const resultBucket = 'document-scanner-ab480.appspot.com';
+		const bucket = 'document-scanner-ab480.appspot.com';
+		// eslint-disable-next-line
+		const owner = auth.currentUser!.uid;
 		const fileName = new Date().getTime() + '.pdf';
-		const url = `gs://${resultBucket}/${fileName}`;
+		const url = `gs://${bucket}/${owner}/${fileName}`;
 
+		// Upload pdf to cloud storage
 		storage
 			.refFromURL(url)
-			.put(pdf.output('blob'), {
-				// eslint-disable-next-line
-				customMetadata: { owner: auth.currentUser!.uid }
-			})
+			.put(pdf.output('blob'))
 			.catch((err) => {
 				console.log(err);
 			});
-		// const uploadData = {
-		// 	token: auth.currentUser?.refreshToken,
-		// 	pdf: pdf.output('arraybuffer')
-		// };
-		// axios.post(
-		// 	'https://us-central1-document-scanner-ab480.cloudfunctions.net/savePdfToDrive',
-		// 	// 'http://localhost:5001/document-scanner-ab480/us-central1/savePdfToDrive',
-		// 	uploadData
-		// );
 	};
 
 	const createPDF = () => {
